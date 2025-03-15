985ea0f795da25f34a0dd3f056791af46d0bd71f
asrouji <72050533+asrouji@users.noreply.github.com>
2025-03-15 11:45:26 -0700

    add Prettier configuration and implement user authentication with Supabase

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,56 @@
 import './App.css'
+import { supabase } from './supabase'
+import { User } from '@supabase/auth-js'
+import { useState, useEffect } from 'react'
 
 function App() {
+  const [user, setUser] = useState<User | null>(null)
+
+  useEffect(() => {
+    const getSession = async () => {
+      const {
+        data: { session },
+      } = await supabase.auth.getSession()
+      setUser(session?.user ?? null)
+    }
+    getSession()
+
+    const { data: authListener } = supabase.auth.onAuthStateChange((_, session) => {
+      setUser(session?.user ?? null)
+    })
+
+    return () => authListener.subscription.unsubscribe()
+  })
+
+  const signInWithGithub = async () => {
+    await supabase.auth.signInWithOAuth({
+      provider: 'github',
+    })
+  }
+
+  const signOut = async () => {
+    await supabase.auth.signOut()
+  }
 
   return (
     <div>
       <h1>HackerChat</h1>
-      <p>Coming Soon...</p>
+      {!user ? (
+        <button onClick={signInWithGithub}>Sign in with GitHub</button>
+      ) : (
+        <div>
+          <p>Welcome, {user.user_metadata.preferred_username}</p>
+          <img
+            src={user.user_metadata.avatar_url}
+            alt="Avatar"
+            style={{
+              width: '30px',
+              borderRadius: '50%',
+            }}
+          />
+          <button onClick={signOut}>Sign Out</button>
+        </div>
+      )}
     </div>
   )
 }
