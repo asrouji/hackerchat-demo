b00086c78db7e25ec624ef484a0e13afc4d71473
asrouji <72050533+asrouji@users.noreply.github.com>
2025-03-14 18:34:00 -0700

    add Supabase types and improve client initialization

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,10 +1,11 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from '@supabase/supabase-js'
+import { Database } from './supabase.types'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
 if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error("Missing env variables for Supabase");
+  throw new Error('Missing env variables for Supabase')
 }
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey)
